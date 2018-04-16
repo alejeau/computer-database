c25d0d23841a1481b1a431d36584344b0fab669d
alejeau <alejeau@excilys.com>
2018-04-16 12:23:56 +0200

    [FEAT] SimpleDAOImpl now using JdbcTemplate.

--- src/main/java/com/excilys/formation/cdb/persistence/dao/impl/SimpleDAOImpl.java
+++ src/main/java/com/excilys/formation/cdb/persistence/dao/impl/SimpleDAOImpl.java
@@ -1,10 +1,12 @@
 package com.excilys.formation.cdb.persistence.dao.impl;
 
 import com.excilys.formation.cdb.exceptions.DAOException;
+import com.excilys.formation.cdb.mapper.jdbcTemplate.JdbcTCompanyMapper;
 import com.excilys.formation.cdb.persistence.dao.SimpleDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Repository;
 
@@ -16,107 +18,42 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
+import static com.excilys.formation.cdb.persistence.dao.impl.CompanyDAORequest.ALL_COMPANIES_WITH_LIMIT;
+
 @Repository
 public class SimpleDAOImpl implements SimpleDAO {
     private static final Logger LOG = LoggerFactory.getLogger(SimpleDAOImpl.class);
 
-    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     SimpleDAOImpl() {
     }
 
     @Autowired
     public SimpleDAOImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
-    public Long count(String query) throws DAOException {
-        LOG.debug("count:");
-        Connection conn = DataSourceUtils.getConnection(dataSource);
-        Statement stmt = null;
-        ResultSet rs = null;
-        Long l = null;
-
-        try {
-            stmt = conn.createStatement();
-
-            LOG.debug("Executing query \"{}\"", query);
-            rs = stmt.executeQuery(query);
-            if (rs.isBeforeFirst()) {
-                while (rs.next()) {
-                    l = rs.getLong(1);
-                }
-            }
-        } catch (SQLException e) {
-            LOG.error("{}", e);
-            throw new DAOException("Couldn't execute the requested COUNT query!", e);
-        } finally {
-            DAOUtils.closeElements(conn, stmt, rs);
-        }
-
-        LOG.debug("Returning {}", l);
-        return l;
+    public Long count(final String QUERY) {
+        LOG.debug("count: {}", QUERY);
+        return jdbcTemplate.queryForObject(QUERY, Long.class);
     }
 
     @Override
-    public Long countElementsWithName(String query, String name) throws DAOException {
+    public Long countElementsWithName(String query, String name) {
         LOG.debug("countElementsWithName");
-        Connection conn = DataSourceUtils.getConnection(dataSource);
-        PreparedStatement prepStmt = null;
-        ResultSet rs = null;
-        Long numberOfElements = null;
 
-        try {
-            prepStmt = conn.prepareStatement(query);
-            prepStmt.setString(1, "%" + name + "%");
-
-            LOG.debug("Executing query \"{}\"", prepStmt);
-            rs = prepStmt.executeQuery();
-            if (rs.isBeforeFirst()) {
-                while (rs.next()) {
-                    numberOfElements = rs.getLong(1);
-                }
-            }
-        } catch (SQLException e) {
-            LOG.error("{}", e);
-            throw new DAOException("Couldn't execute the requested COUNT LIKE query!", e);
-        } finally {
-            DAOUtils.closeElements(conn, prepStmt, rs);
-        }
-
-        LOG.debug("Returning {}", numberOfElements);
-        return numberOfElements;
+        Object[] param = new Object[]{"%" + name + "%"};
+        return jdbcTemplate.queryForObject(query, param, Long.class);
     }
 
     public Long countWithStringParameters(String query, List<String> params) throws DAOException {
         LOG.debug("countWithStringParameters");
-        Connection conn = DataSourceUtils.getConnection(dataSource);
-        PreparedStatement prepStmt = null;
-        ResultSet rs = null;
-        Long numberOfElements = null;
-
-        try {
-            prepStmt = conn.prepareStatement(query);
-            for (int i = 0; i < params.size(); i++) {
-                prepStmt.setString(i + 1, "%" + params.get(i) + "%");
-            }
-
-            LOG.debug("Executing query \"{}\"", prepStmt);
-            rs = prepStmt.executeQuery();
-            if (rs.isBeforeFirst()) {
-                while (rs.next()) {
-                    numberOfElements = rs.getLong(1);
-                }
-            }
-        } catch (SQLException e) {
-            LOG.error("{}", e);
-            throw new DAOException("Couldn't execute the requested COUNT LIKE query!", e);
-        } finally {
-            DAOUtils.closeElements(conn, prepStmt, rs);
+        Object[] objects = new Object[params.size()];
+        for (int i = 0; i < params.size(); i++) {
+            objects[i] = params.get(i);
         }
-
-        LOG.debug("Returning {}", numberOfElements);
-        return numberOfElements;
+        return jdbcTemplate.queryForObject(query, objects, Long.class);
     }
 }
