3cd09b4702347a380e416f4d836fe9e68948ca25
alejeau <alejeau@excilys.com>
2018-04-17 14:05:05 +0200

    [FIX] Corrected error message.

--- src/main/java/com/excilys/formation/cdb/validators/core/ErrorMessage.java
+++ src/main/java/com/excilys/formation/cdb/validators/core/ErrorMessage.java
@@ -10,5 +10,5 @@ public class ErrorMessage {
     public static final String CANT_BE_EMPTY = "can't be empty!";
     public static final String CANT_START_WITH_SPACE = "can't start with space (' ') char !";
     public static final String MUST_FOLLOW_DATE_PATTERN = "must follow the pattern \"" + DatePattern.PATTERN + "\"!";
-    public static final String INTRO_DATE_MUST_BE_BEFORE_DISCONTINUATION_DATE = "the date of introduction must be before the discontinuation date!";
+    public static final String INTRO_DATE_MUST_BE_BEFORE_DISCONTINUATION_DATE = "the date of introduction must be set before the discontinuation date!";
 }
