fa38c0931fea1a18400bfd11569b34d44e789076
alejeau <alejeau@excilys.com>
2018-04-20 15:12:22 +0200

    [FIX] Reverted to hardcoded constants in controllers.constants.Paths.

--- src/main/java/com/excilys/formation/cdb/controllers/constants/Paths.java
+++ src/main/java/com/excilys/formation/cdb/controllers/constants/Paths.java
@@ -1,34 +1,12 @@
 package com.excilys.formation.cdb.controllers.constants;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Properties;
-
 public class Paths {
-    private static final Logger LOG = LoggerFactory.getLogger(Paths.class);
-    private static Properties properties = new Properties();
-    private static final String PROPERTIES_FILE = "/properties/url.properties";
+    public static final String LOCAL_PATH_DASHBOARD = "/access";
+    public static final String LOCAL_PATH_ADD_COMPUTER = "/access/computer/add";
+    public static final String LOCAL_PATH_EDIT_COMPUTER = "/access/computer/edit";
+    public static final String LOCAL_PATH_SEARCH_COMPUTER = "/access/computer/search";
 
-    static {
-        try {
-            properties.load(Paths.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE));
-        } catch (IOException e) {
-            LOG.error("Couldn't properties file \"{}\"!", PROPERTIES_FILE);
-            Arrays.stream(e.getStackTrace())
-                    .map(StackTraceElement::toString)
-                    .forEach(LOG::error);
-        }
-    }
-
-    public static final String LOCAL_PATH_DASHBOARD = properties.getProperty("url.dashboard");
-    public static final String LOCAL_PATH_ADD_COMPUTER = properties.getProperty("url.add-computer");
-    public static final String LOCAL_PATH_EDIT_COMPUTER = properties.getProperty("url.edit-computer");
-    public static final String LOCAL_PATH_SEARCH_COMPUTER = properties.getProperty("url.search-computer");
-
-    public static final String PREFIX = properties.getProperty("url.prefix");
+    public static final String PREFIX = "/computer-database";
     public static final String ABSOLUTE_PATH_DASHBOARD = String.format("%s%s", PREFIX, LOCAL_PATH_DASHBOARD);
     public static final String ABSOLUTE_PATH_ADD_COMPUTER = String.format("%s%s", PREFIX, LOCAL_PATH_ADD_COMPUTER);
     public static final String ABSOLUTE_PATH_EDIT_COMPUTER = String.format("%s%s", PREFIX, LOCAL_PATH_EDIT_COMPUTER);
