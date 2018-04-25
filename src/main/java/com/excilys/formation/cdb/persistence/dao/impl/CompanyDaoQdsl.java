55c55bb4e137a337aef8abe5a7dd6576c262caea
alejeau <alejeau@excilys.com>
2018-04-25 15:22:41 +0200

    [FEAT](dao) Queries in CompayDaoQdsl are now done.

--- src/main/java/com/excilys/formation/cdb/persistence/dao/impl/CompanyDaoQdsl.java
+++ src/main/java/com/excilys/formation/cdb/persistence/dao/impl/CompanyDaoQdsl.java
@@ -3,74 +3,92 @@ package com.excilys.formation.cdb.persistence.dao.impl;
 import com.excilys.formation.cdb.model.Company;
 import com.excilys.formation.cdb.model.QCompany;
 import com.excilys.formation.cdb.persistence.dao.CompanyDAO;
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.querydsl.jpa.hibernate.HibernateDeleteClause;
+import com.querydsl.jpa.hibernate.HibernateQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class CompanyDaoQdsl implements CompanyDAO {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-    private JPAQueryFactory queryFactory;
+    private SessionFactory sessionFactory;
     private QCompany qCompany;
 
-    public CompanyDaoQdsl() {
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(DAOUtils.EMF_NAME);
-//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("com.excilys.formation.cdb.config.HibernatePersistenceConfigCLI");
-//        EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        Session session;
-//        session.get
-        this.queryFactory = new JPAQueryFactory(entityManager);
+    @Autowired
+    public CompanyDaoQdsl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
         this.qCompany = QCompany.company;
     }
 
     @Override
     public Long getNumberOfCompanies() {
-        return queryFactory.selectFrom(qCompany)
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> query = new HibernateQuery<>(session);
+        Long result = query.select(qCompany.id)
+                .from(qCompany)
                 .fetchCount();
+        session.close();
+        return result;
     }
 
     @Override
     public Long getNumberOfCompaniesWithName(String name) {
-        return queryFactory.selectFrom(qCompany)
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> query = new HibernateQuery<>(session);
+        Long result = query.select(qCompany.id)
+                .from(qCompany)
                 .where(qCompany.name.eq(name))
                 .fetchCount();
+        session.close();
+        return result;
     }
 
     @Override
     public Company getCompany(Long id) {
-        return queryFactory.selectFrom(qCompany)
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> query = new HibernateQuery<>(session);
+        Company result = query.select(qCompany)
+                .from(qCompany)
                 .where(qCompany.id.eq(id))
                 .fetchOne();
+        session.close();
+        return result;
     }
 
     @Override
     public List<Company> getCompanies() {
-        return queryFactory.selectFrom(qCompany)
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> query = new HibernateQuery<>(session);
+        List<Company> result = query.select(qCompany)
+                .from(qCompany)
+                .orderBy(qCompany.name.asc())
                 .fetch();
+        session.close();
+        return result;
     }
 
     @Override
     public List<Company> getCompanies(Long index, Long limit) {
-        return queryFactory.selectFrom(qCompany)
+        Session session = sessionFactory.openSession();
+        HibernateQuery<Company> query = new HibernateQuery<>(session);
+        List<Company> result = query.select(qCompany)
+                .from(qCompany)
                 .offset(index)
                 .limit(limit)
                 .orderBy(qCompany.name.asc())
                 .fetch();
+        session.close();
+        return result;
     }
 
     @Override
     public void deleteCompany(Long id) {
-        queryFactory.delete(qCompany)
-                .where(qCompany.id.eq(id))
-                .execute();
+        Session session = sessionFactory.openSession();
+        new HibernateDeleteClause(session, qCompany).where(qCompany.id.eq(id)).execute();
+        session.close();
     }
 }
