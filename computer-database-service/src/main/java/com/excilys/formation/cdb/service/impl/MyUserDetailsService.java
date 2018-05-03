29af2797a314ccfdaab4679b25d6234734efee2c
Aurélien LEJEAU <alejeau@excilys.com>
2018-05-03 14:25:35 +0200

    [FEAT] MyUserDetailsService is now a @Service.

--- computer-database-service/src/main/java/com/excilys/formation/cdb/service/impl/MyUserDetailsService.java
+++ computer-database-service/src/main/java/com/excilys/formation/cdb/service/impl/MyUserDetailsService.java
@@ -10,13 +10,17 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+@Service("userDetailsService")
+@Transactional
 public class MyUserDetailsService implements UserDetailsService {
 
     private UserService userService;
