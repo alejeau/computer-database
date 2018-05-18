731aa7480026dc614c45bfbc9aaf9f54a77b2bb5
alejeau <alejeau@excilys.com>
2018-05-18 17:39:41 +0200

    [FEAT] Added CORS filter.

--- computer-database-webservice/src/main/java/com/excilys/formation/cdb/rest/impl/CompanyRestController.java
+++ computer-database-webservice/src/main/java/com/excilys/formation/cdb/rest/impl/CompanyRestController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(Paths.REST_COMPANY)
 public class CompanyRestController implements CompanyRest {
     private static final Logger LOG = LoggerFactory.getLogger(CompanyRestController.class);
--- computer-database-webservice/src/main/java/com/excilys/formation/cdb/rest/impl/ComputerRestController.java
+++ computer-database-webservice/src/main/java/com/excilys/formation/cdb/rest/impl/ComputerRestController.java
@@ -18,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import java.util.List;
 import java.util.Objects;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(Paths.REST_COMPUTER)
 public class ComputerRestController implements ComputerRest {
     private static final Logger LOG = LoggerFactory.getLogger(ComputerRestController.class);
