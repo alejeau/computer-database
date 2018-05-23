905d5187c88f706169b0fc03d11aa30e3a85d7bf
Samia Nabili <snabili@excilys.com>
2018-05-23 16:42:52 +0200

    [FIX] error in getUserByName request

--- computer-database-persistence/src/main/java/com/excilys/formation/cdb/persistence/dao/impl/UserDaoQdsl.java
+++ computer-database-persistence/src/main/java/com/excilys/formation/cdb/persistence/dao/impl/UserDaoQdsl.java
@@ -8,8 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.excilys.formation.cdb.model.QUser;
-import com.excilys.formation.cdb.model.User;
+import com.excilys.formation.cdb.model.QUserInfo;
+import com.excilys.formation.cdb.model.UserInfo;
 import com.querydsl.jpa.hibernate.HibernateQuery;
 
 @Repository
@@ -18,23 +18,21 @@ public class UserDaoQdsl {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ComputerDaoQdsl.class);
 	private SessionFactory sessionFactory;
-	private QUser qUser;
+	private QUserInfo qUserInfo;
 
 	@Autowired
-	public UserDaoQdsl (SessionFactory sessionFactory) {
+	public UserDaoQdsl(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
-		this.qUser = QUser.user;
+		this.qUserInfo = QUserInfo.userInfo;
 	}
 
-	public User getUserByUsername(String username) {
-		 LOG.debug("getUserByUsername {}");
-	        Session session = sessionFactory.openSession();
-	        HibernateQuery<User> query = new HibernateQuery<>(session);
-	        User user = (User) query.select()
-	                .from(qUser)
-	                .where(qUser.username.eq(username)).fetch();
-	        session.close();
-	        LOG.debug("Returning {}", user);
-	        return user;
+	public UserInfo getUserByUsername(String username) {
+		LOG.debug("getUserByUsername {}", username);
+		Session session = sessionFactory.openSession();
+		HibernateQuery<UserInfo> query = new HibernateQuery<>(session);
+		UserInfo user = query.from(qUserInfo).where(qUserInfo.username.eq(username)).fetchOne();
+		session.close();
+		LOG.debug("Returning {}", user);
+		return user;
 	}
 }
