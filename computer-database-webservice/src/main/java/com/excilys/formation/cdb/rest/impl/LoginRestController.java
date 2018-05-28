774464f3f81c50b3d6e61e8b8b4dc99ae5670383
alejeau <alejeau@excilys.com>
2018-05-28 16:02:09 +0200

    [FIX] Removed unused parameter in login method.

--- computer-database-webservice/src/main/java/com/excilys/formation/cdb/rest/impl/LoginRestController.java
+++ computer-database-webservice/src/main/java/com/excilys/formation/cdb/rest/impl/LoginRestController.java
@@ -1,11 +1,8 @@
 package com.excilys.formation.cdb.rest.impl;
 
-import java.util.Optional;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.excilys.formation.cdb.model.UserInfo;
+import com.excilys.formation.cdb.model.constants.Paths;
+import com.excilys.formation.cdb.security.config.SimpleAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,31 +11,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.excilys.formation.cdb.model.UserInfo;
-import com.excilys.formation.cdb.model.constants.Paths;
-import com.excilys.formation.cdb.security.config.SimpleAuthenticationService;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(Paths.REST_LOGIN)
 public class LoginRestController {
 
-	private SimpleAuthenticationService authentication;
+    private SimpleAuthenticationService authentication;
 
-	@Autowired
-	public LoginRestController(SimpleAuthenticationService authService) {
-		this.authentication = authService;
-	} 
+    @Autowired
+    public LoginRestController(SimpleAuthenticationService authService) {
+        this.authentication = authService;
+    }
 
-	@PostMapping
-	public String login(HttpServletRequest request, HttpServletResponse response, @RequestBody UserInfo user) {
-		Optional<String> authResultOpt =  authentication.login(user.getUsername(), user.getPassword());
-		if (!authResultOpt.isPresent()) {
-			response.setStatus(HttpStatus.UNAUTHORIZED.value());
-			return null;
-		}
-		return authResultOpt.get();
-	}
-	 
+    @PostMapping
+    public String login(HttpServletResponse response, @RequestBody UserInfo user) {
+        Optional<String> authResultOpt = authentication.login(user.getUsername(), user.getPassword());
+        if (!authResultOpt.isPresent()) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return null;
+        }
+        return authResultOpt.get();
+    }
+
 
 }
